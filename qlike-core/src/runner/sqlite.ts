e2fda3bbbcc9a8f5755066785b6f433576830051
ismail codar <icodar@gmail.com>
2021-08-03 06:59:50 +0300

    Pool.ts ...

--- qlike-core/src/runner/sqlite.ts
+++ qlike-core/src/runner/sqlite.ts
@@ -1,4 +1,5 @@
 import sqlite3 from 'sqlite3';
+import { Pool } from './pool';
 
 const db = new sqlite3.Database(':memory:');
 
@@ -17,3 +18,24 @@ db.serialize(function () {
 });
 
 db.close();
+
+// TODO https://github.com/mysqljs/mysql/blob/master/lib/Pool.js https://github.com/hyurl/better-sqlite-pool/blob/master/index.js
+
+const pool = new Pool(null);
+
+function getQuery(query) {
+  var promise = new Promise(function (resolve, reject) {
+    pool.acquire().then((connection) => {
+      console.log('Connected as id ' + connection.threadId);
+      connection.query(query, (error, results) => {
+        connection.release();
+        if (error) {
+          reject('Error in database operation');
+        } else {
+          resolve(results);
+        }
+      });
+    });
+  });
+  return promise;
+}
